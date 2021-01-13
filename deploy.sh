77c3beedbe65326c1f277aba4359611d2ed9f7c3
Devin <zhangchuzhao@dingtalk.com>
2021-01-13 18:54:54 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo start deploy...
 
 kill -HUP `cat api_server.pid`
 kill `cat api_server.pid`
-gunicorn api_server:app -p api_server.pid -b 0.0.0.0:5000 -w 4 -D
+gunicorn api_server:app -p api_server.pid -b 0.0.0.0:5000 -D
 
 echo finish deploy...
